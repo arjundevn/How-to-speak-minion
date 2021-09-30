04b48b5fd60457aa36cf8ef8444d1351747ea74a
arjundevn <arjun.dev.n@gmail.com>
2021-09-30 17:43:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector(".btn-submit");
+
+var convertText = document.querySelector("#input")
+
+btn.addEventListener("click", function clickEventHandle() {
+    console.log("Hell");
+    console.log(convertText.value);
+});
+
